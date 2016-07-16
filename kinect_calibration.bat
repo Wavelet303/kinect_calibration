54c170972b6701051e26168e05d854f469812b57
hwdong <hwdong.cn@gmail.com>
2016-07-16 10:17:23 +0800

    windows bat file to run the program

---
+++ kinect_calibration.bat
@@ -0,0 +1,13 @@
+cd
+rem kinect_capture  -w 7 -h 5 -ir 0
+rem kinect_capture  -w 7 -h 5 -ir 1
+
+::带有-s的表示直接从文件读取图像进行标定，如果此时-ir 值是1则表示读取红外图像对红外相机标定
+rem kinect_capture -w 7 -h 5 -s 0.03 -ir 0 -dir calib/intrinsic -o camera.ymal
+rem kinect_capture -w 7 -h 5 -s 0.03 -ir 1 -dir calib/intrinsic -o ir_camera.ymal
+
+::ex 1表示进行外部标定，求得2个相机（彩色和红外相机）的相对位置
+rem kinect_capture  -w 7 -h 5 -s 0.03 -ex 1 -ip image_pairs.xml -o ex_camera.ymal
+kinect_capture  -w 7 -h 5 -s 0.03 -ex 1 -ip image_pairs.txt -o ex_camera.ymal
+
+pause
